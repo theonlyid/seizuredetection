9f38b7836f1d3038c6b744b8181a8fc10c650baf
Ali Zaidi <ali.zaidi@wysscenter.ch>
2019-03-15 18:12:42 +0100

    Analysis is source code for ipython notebook

---
+++ src/analysis.py
@@ -0,0 +1,18 @@
+#%% [markdown]
+# # Spectral decomposition based Seizure detection
+#
+# Author: Ali Zaidi
+# 
+# ### The dataset
+#
+# This is a portion of the Temple University Dataset.
+# Our Objective is to train a classifier to detect seizures fron EEG data.
+
+#%%
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+plt.plot(np.ones((1,100)))
+plt.show()
+#%% [markdown]
